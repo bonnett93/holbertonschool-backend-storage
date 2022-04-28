4ca0dd2f7f9e7554a60f3892f55ed5cbf959bf0f
bonnett93 <bonnettjordan@gmail.com>
2022-04-28 14:54:16 -0500

    Upload task 4

---
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -0,0 +1,7 @@
+-- creates a trigger that decreases the quantity of an item after adding a new order.
+-- mysql query
+CREATE TRIGGER update_items AFTER INSERT ON orders
+FOR EACH ROW
+	UPDATE items
+		SET quantity = quantity - NEW.number
+	WHERE name = NEW.item_name;
