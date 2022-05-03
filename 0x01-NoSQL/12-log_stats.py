a1e93762ea2c455349132538403b60da06277bb1
bonnett93 <bonnettjordan@gmail.com>
2022-05-03 11:01:22 -0500

    try print

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -12,17 +12,17 @@ if __name__ == "__main__":
     logs = nginx_collection.count_documents({})
     print(f"{logs} logs")
 
-    get = nginx_collection.count_documents({"method": "GET"})
-    post = nginx_collection.count_documents({"method": "POST"})
-    put = nginx_collection.count_documents({"method": "PUT"})
-    patch = nginx_collection.count_documents({"method": "PATCH"})
-    delete = nginx_collection.count_documents({"method": "DELETE"})
-    print(f"""Methods:
-    \tmethod GET: {get}
-    \tmethod POST: {post}
-    \tmethod PUT: {put}
-    \tmethod PATCH: {patch}
-    \tmethod DELETE: {delete}""")
+    # get = nginx_collection.count_documents({"method": "GET"})
+    # post = nginx_collection.count_documents({"method": "POST"})
+    # put = nginx_collection.count_documents({"method": "PUT"})
+    # patch = nginx_collection.count_documents({"method": "PATCH"})
+    # delete = nginx_collection.count_documents({"method": "DELETE"})
+
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    print("Methods:")
+    for method in methods:
+        count = nginx_collection.count_documents({"method": method})
+        print(f'\tmethod {method}: {count}')
 
     get_status_check = nginx_collection.count_documents({
         "method": "GET",
