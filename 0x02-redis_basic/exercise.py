2ddec3aad107f707bcf718f366cf4f866395ff0f
bonnett93 <bonnettjordan@gmail.com>
2022-05-04 17:20:57 -0500

    Upload task 1

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,7 +4,7 @@
 """
 import redis
 import uuid
-from typing import Union
+from typing import Union, Optional, Callable, Any
 
 
 class Cache:
@@ -23,3 +23,30 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str,
+            fn: Optional[Callable[..., Any]]) -> Union[str, bytes, int, float]:
+        """
+        take a key string argument and an optional Callable argument named fn.
+        This callable will be used to convert the data back to the
+        desired format.
+        """
+        value = self._redis.get(key)
+        if fn:
+            value = fn(value)
+        return value
+
+    def get_str(self, key: str) -> str:
+        """parametrize Cache.get"""
+        value = self._redis.get(key)
+        return value.decode("utf-8")
+
+    def get_int(self, key) -> int:
+        """ parametrize Cache.get"""
+        value = self._redis.get(key)
+        try:
+            value = int(value.decode("utf-8"))
+        except ValueError:
+            value = None
+        finally:
+            return value
