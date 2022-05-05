d71cb3645ef00c32bab0a4b3ed2c40696170cb90
bonnett93 <bonnettjordan@gmail.com>
2022-05-04 22:32:24 -0500

    print called times

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -43,17 +43,18 @@ def call_history(method: Callable) -> Callable:
 def replay(method: Callable) -> None:
     """display the history of calls of a particular function."""
     my_redis = redis.Redis()
-    called_times = int(my_redis.get(method.__qualname__))
+    key = method.__qualname__
+    called_times = int(my_redis.get(key))
 
-    inputs_key = f'{method.__qualname__}:inputs'
+    inputs_key = f'{key}:inputs'
     inputs_values = my_redis.lrange(inputs_key, 0, -1)
 
-    outputs_key = f'{method.__qualname__}:outputs'
+    outputs_key = f'{key}:outputs'
     outputs_values = my_redis.lrange(outputs_key, 0, -1)
 
+    print(f'{key} was called {called_times} times:')
     for _input, _output in zip(inputs_values, outputs_values):
-        print('{}(*{}) -> {}'.format(method.__qualname__,
-                                     _input.decode("utf-8"),
+        print('{}(*{}) -> {}'.format(key, _input.decode("utf-8"),
                                      _output.decode("utf-8")))
 
 
